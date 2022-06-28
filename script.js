58cddfddb80001430f53fbee6f1f66e853a9df66
gustavomatiass <gustavomatiass123@gmail.com>
2022-06-28 10:46:19 -0300

    menu

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type == 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarge.setAttribute('aria-expanded', 'active');
+    if(active) {
+        event.currentTarge.setAttribute('aria-label', 'fechar menu');
+    } else {
+        event.currentTarge.setAttribute('aria-label', 'abrir menu');
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
